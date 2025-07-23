51f5155510c7bd4ffab11fd8d0dbf0cf10911cd2
Aline_Munezero <alinemunezero920@gmail.com>
2025-07-23 09:26:12 +0200

    Update solution - CodeHub

--- 5-kyu/simple-events/simple-events.js
+++ 5-kyu/simple-events/simple-events.js
@@ -1,22 +1,20 @@
 function Event(){
     const handlers=[];
-    return {
-        subscribe: function(fn){
-            handlers.push(fn);
-        },
-       unsubscribe: function(fn){
-            const index= handlers.indexOf(fn);
-            if(index !== -1){
-                handlers.splice(index,1);
-            }
-        },
-        
-        emit:function(...args){
-            handlers.forEach(fn=>{
-                fn(...args)
-            })
-        }
-       
-    };
+   return {
+       subscribe: fn=>{
+           handlers.push(fn);
+       },
+       unsubscribe: fn=>{
+           const index= handlers.indexOf(fn);
+           if(index !== -1){
+               handlers.splice(index,1);
+           }
+       },
+      
+       emit:(...args)=>{
+           handlers.forEach(fn=>{
+               fn(...args);
+           })
+       },
+   }
 }
-​
